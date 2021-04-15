ba202dc5925bbe5bb6095e7105bfcc0788578e4d
wj <343251455@qq.com>
2021-04-15 10:57:03 +0800

    a

--- util/component/axes.js
+++ util/component/axes.js
@@ -6,29 +6,6 @@ export default class Axes{
     constructor(cx,cy){
         this.cx = cx;
         this.cy = cy;
-        this.xLine = new line([
-            new Vector(-innerWidth/2,0),
-            new Vector(innerWidth/2,0)
-        ],{
-            borderWidth:2,
-            borderColor:Color.RED
-        });
-        this.yLine = new line([
-            new Vector(0,-innerHeight / 2),
-            new Vector(0,innerHeight / 2)
-        ],{
-            borderWidth:2,
-            borderColor:Color.GREEN
-        });
-    }
-    setOrigin(x,y){
-        this.xLine.setOrigin(x,y);
-        this.yLine.setOrigin(x,y);
-    }
-    render(pen){
         
-        
-        this.xLine.render(pen);
-        this.yLine.render(pen);
     }
 }
--- util/component/grid.js
+++ util/component/grid.js
@@ -6,50 +6,9 @@ export default class Grid {
     constructor(sx,sy){
         this.sx = sx;
         this.sy = sy;
-        this.xLine = [];
-        this.yLine = [];
-        for(let i=1;i<innerHeight/2;i+=this.sx){
-            
-                this.xLine.push(
-                    new line([
-                        new Vector(-innerWidth / 2,i),
-                        new Vector(innerWidth / 2,i)
-                    ],{
-                        borderColor:Color.RED
-                    })
-                    ,
-                    new line([
-                        new Vector(-innerWidth / 2,-i),
-                        new Vector(innerWidth / 2,-i)
-                    ],{
-                        borderColor:Color.RED
-                    })
-                );
-        }
-        for(let j=0;j<innerWidth/2;j+=this.sy){
-            this.yLine.push(
-                new line([
-                    new Vector(j,-innerHeight/2),
-                    new Vector(j,innerHeight/2),
-                ],{
-                    borderColor:Color.GREEN
-                })
-                ,
-                new line([
-                    new Vector(-j,-innerHeight/2),
-                    new Vector(-j,innerHeight/2),
-                ],{
-                    borderColor:Color.GREEN
-                })
-            )
-        }
+
     }
     render(pen){
-        this.xLine.forEach(d => {
-            d.render(pen);
-        })
-        this.yLine.forEach(d => {
-            d.render(pen);
-        })
+        
     }
 }
